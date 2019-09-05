a9022d7557348d2321de4a1e62067b1a9d89180d
Juan Muñoz <cl.jmunoz@gmail.com>
2019-09-05 17:29:50 -0400

    Primera carga

---
+++ JavaScript/main.js
@@ -0,0 +1,37 @@
+var inputs = document.getElementsByClassName('formulario-input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('keyup',function(){
+        if(this.value.length>=1) {
+            this.nextElementSibling.classList.add('fijar');
+        }else{
+            this.nextElementSibling.classList.remove('fijar');
+        }
+    });
+}
+
+
+
+function modifyText() {
+    var letters = 
+                [   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+                    "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", 
+                    "v", "w", "x", "y", "z", ",", ".", "!", "'", "$", "%", "&",
+                    "/", "(", ")", "=", "?", "¿", "¡", ".", ","
+                ]
+    var txtRut = document.getElementById("txtRut")
+    txtRut.value = txtRut.value.toLowerCase()
+    for(let i in letters){
+        txtRut.value = txtRut.value.replace(letters[i], "")
+    }
+    if(txtRut.value.includes("-")) {
+        txtRut.value = txtRut.value.replace(/-/g, "")
+        txtRut.value = txtRut.value.slice(0, txtRut.value.length - 1) + "-" + txtRut.value.slice(txtRut.value.length - 1)
+    }else
+    {
+        txtRut.value = txtRut.value.slice(0, txtRut.value.length - 1) + "-" + txtRut.value.slice(txtRut.value.length - 1)
+    }
+    if(txtRut.value.includes("k")) {
+        txtRut.value = txtRut.value.replace(/k/g, "")
+        txtRut.value += "k"
+    }
+}
