f3872a1f931978a76d8826ff2f631fc8f73a85c1
Juan Muñoz <cl.jmunoz@gmail.com>
2019-10-25 12:35:56 -0300

    Se cambia vista.

--- SitioColegios/SitioColegios/SitioColegios/views.py
+++ SitioColegios/SitioColegios/SitioColegios/views.py
@@ -1,7 +1,13 @@
 from django.http import  HttpResponse
+from django.template import Template, Context
 
 def saludo(request):
-     return HttpResponse("Hola que hace") 
+     doc_externo=open("/Users/juanshocl/Documents/GitHub/SitioColegios/SitioColegios/SitioColegios/SitioColegios/Templates/index.html")
+     plt_index = Template(doc_externo.read())      
+     doc_externo.close()
+     contexto  = Context()
+     documento= plt_index.render(contexto)
+     return HttpResponse(documento) 
 
 def despedida(request):
      return HttpResponse("Adios") 
