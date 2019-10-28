ab29166641c77a98bc44cf588a75e29b21d3f19b
Juan Muñoz <cl.jmunoz@gmail.com>
2019-10-27 22:03:11 -0300

    Carga de plantillas

--- SitioColegios/SitioColegios/urls.py
+++ SitioColegios/SitioColegios/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 #from SitioColegios.views import index, despedida, ingreso, formulario
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
     path ('catalog/', include('catalog.urls')), #aplicacion de catalogo de colegios.
     #path('', views.index, name='home'),
 ]
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
