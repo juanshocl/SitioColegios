072239e1b04eb215396e73a7738d7d8f8401fa5c
Juan Muñoz <cl.jmunoz@gmail.com>
2019-11-16 15:16:51 -0300

    Vista basada en Clases

--- SitioColegios/SitioColegios/urls.py
+++ SitioColegios/SitioColegios/urls.py
@@ -18,7 +18,9 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
-from SitioColegios.views import index, school_delete, school_editar, school_listar, evaluar, galeria, index, school_view
+from SitioColegios.views import SchoolList, SchoolCreate, SchoolUpdate, SchoolDelete, RatingsCreate
+#from SitioColegios.views import index, school_delete, school_editar, school_listar, evaluar, galeria, index, school_view, SchoolList, \
+ #   SchoolCreate, SchoolUpdate, SchoolDelete, RatingsCreate
 from . import views
 
 urlpatterns = [
@@ -26,12 +28,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'), #Vista estatica del index
     path('galeria/', views.galeria, name='galeria'),
-    path('evaluar/', views.evaluar, name='crear_evaluacion'),
-    path('listar/', views.school_listar, name='school_listar'),
+    path('evaluar/', RatingsCreate.as_view(), name='crear_evaluacion'),
+    path('listar/', SchoolList.as_view(), name='school_listar'),
     #url(r'^editar/(?P<pk>\d+)/$', views.school_listar, name='school_listar'),
     # url(r'^editar/(?P<id_School>\d+)/$', views.school_editar, name='school_editar'),
-    path('editar/<id_School>', views.school_editar, name='school_editar'),
-    path('nuevo/', views.school_view, name='school_view'),
-    path('eliminar/<id_School>', views.school_delete, name='school_delete'),
+    path('editar/<pk>', SchoolUpdate.as_view(), name='school_editar'),
+    path('nuevo/', SchoolCreate.as_view(), name='school_view'),
+    path('eliminar/<pk>', SchoolDelete.as_view(), name='school_delete'),
     #path ('catalog/', include('catalog.urls')), #aplicacion de catalogo de colegios.
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- SitioColegios/SitioColegios/views.py
+++ SitioColegios/SitioColegios/views.py
@@ -2,6 +2,10 @@ from django.http import  HttpResponse
 from django.template import Template, Context
 from django.shortcuts import render, redirect
 from django.db.models import Avg
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.db.models import F
+
 
 from catalog.models import SchoolType, User, state_province, School, Ratings
 from catalog.forms import SchoolForm, RatingsForm
@@ -22,65 +26,112 @@ def galeria(request):
      'galeria/galeria.html',
      context={},
 )
-def school_listar(request):
-     School_instance = School.objects.all().order_by('Name')
-     loop_range = range (1,6)
-     contexto = {'School_instance':School_instance, 'loop_range': loop_range}
-     return render(
-          request,
-          'admin/school_list.html',
-          contexto,
-     )
+# def school_listar(request):
+#      School_instance = School.objects.all().order_by('Name')
+#      loop_range = range (1,6)
+#      contexto = {'School_instance':School_instance, 'loop_range': loop_range}
+#      return render(
+#           request,
+#           'admin/school_list.html',
+#           contexto,
+#      )
 
-def school_editar(request, id_School):
-     School_instance = School.objects.get(Id=id_School)
-     if request.method == 'GET':
-          form = SchoolForm(instance=School_instance)
-     else:
-          form = SchoolForm(request.POST, instance=School_instance)
-          if form.is_valid():
-               form.save(request.POST, request.FILES)
-          return redirect('school_listar')
-     return render(request, 'admin/school_form.html', {'form':form})
+# def school_editar(request, id_School):
+#      School_instance = School.objects.get(Id=id_School)
+#      if request.method == 'GET':
+#           form = SchoolForm(instance=School_instance)
+#      else:
+#           form = SchoolForm(request.POST, instance=School_instance)
+#           if form.is_valid():
+#                form.save(request.POST, request.FILES)
+#           return redirect('school_listar')
+#      return render(request, 'admin/school_form.html', {'form':form})
 
 def formulario(request):
     return HttpResponse("Contacto")
 
-def evaluar(request):
-     if request.method == 'POST':
-          form = RatingsForm(request.POST)
-          if form.is_valid():
-               form.save()
-          return redirect('index')
-     else:
-          form = RatingsForm()
+# def evaluar(request):
+#      if request.method == 'POST':
+#           form = RatingsForm(request.POST)
+#           if form.is_valid():
+#                form.save()
+#           return redirect('index')
+#      else:
+#           form = RatingsForm()
 
-     return render(
-     request,
-     'evaluar/evaluacion.html',
-     context={'form': form},
-)
-def school_view(request):
-     if request.method == 'POST':
-          form = SchoolForm(request.POST, request.FILES)
-          if form.is_valid():
-               form.save()
+#      return render(
+#      request,
+#      'evaluar/evaluacion.html',
+#      context={'form': form},
+# )
+# def school_view(request):
+#      if request.method == 'POST':
+#           form = SchoolForm(request.POST, request.FILES)
+#           if form.is_valid():
+#                form.save()
 
-          return redirect('index')
-     else:
-          form = SchoolForm()
+#           return redirect('listar')
+#      else:
+#           form = SchoolForm()
 
-     return render(
-     request,
-     'admin/school_form.html', 
-     context={'form': form},
-     )
+#      return render(
+#      request,
+#      'admin/school_form.html', 
+#      context={'form': form},
+#      )
 
-def school_delete(request, id_School):
-     colegio = School.objects.get(Id=id_School)
-     if request.method == 'POST':
-          colegio.delete()
-          return redirect('school_listar')
-     return render(request, 
-     'admin/school_delete.html',
-      {'colegio': colegio})
+# def school_delete(request, id_School):
+#      colegio = School.objects.get(Id=id_School)
+#      if request.method == 'POST':
+#           colegio.delete()
+#           return redirect('school_listar')
+#      return render(request, 
+#      'admin/school_delete.html',
+#       {'colegio': colegio})
+
+class SchoolList(ListView):
+     model = School
+     template_name = 'admin/school_list.html'
+
+class SchoolCreate(CreateView):
+     model = School
+     form_class = SchoolForm
+     template_name = 'admin/school_form.html'
+     success_url = reverse_lazy('school_listar')
+
+class SchoolUpdate(UpdateView):
+     model = School
+     form_class = SchoolForm
+     template_name = 'admin/school_form.html'
+     success_url = reverse_lazy('school_listar')
+     
+class SchoolDelete(DeleteView):
+     model = School
+     template_name  = 'admin/school_delete.html'
+     success_url  = reverse_lazy('school_listar')
+
+class RatingsList(ListView):
+     model = Ratings
+     template_name = 'evaluar/evaluacion.html'
+
+class RatingsCreate(CreateView):
+     model = Ratings
+     model_shool = School
+     form_class = RatingsForm
+     # second_form_class = SchoolForm
+     #dato = form_class.Score
+     template_name = 'evaluar/evaluacion_list.html'
+     success_url  = reverse_lazy('index')
+
+     # def get_context_data(self, **kwargs):
+     #      context = super(RatingsCreate, self).get_context_data(**kwargs)
+     #      if 'form' not in context:
+     #           context['form'] = self.form_class(self.request.GET)
+     #      if 'form2' not in context:
+     #           context['form2'] = self.form_class(self.request.GET)
+     #      return context
+     
+     # def post(self, request, **args, **kwargs):
+     #      self.object = self.get_object
+     #      form = self.form_class(request.POST)
+     #      form2 = self.second_form_class(request.POST)
