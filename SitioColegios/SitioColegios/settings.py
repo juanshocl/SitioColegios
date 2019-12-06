3467a15588ca297fcd39144de0066dcc48983990
Juan Muñoz <cl.jmunoz@gmail.com>
2019-12-06 01:42:19 -0300

    listo para heroku

--- SitioColegios/SitioColegios/settings.py
+++ SitioColegios/SitioColegios/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import os
 from unipath import Path
 from django.urls import reverse_lazy
 
